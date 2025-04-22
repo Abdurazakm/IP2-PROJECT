049af024138f4f1c15730b8ed44ba572af84b76f
Abdurazakm <abdulkem5472@gmail.com>
2025-04-22 12:32:47 +0300

    connection database

--- assets/backend/config/database.php
+++ assets/backend/config/database.php
@@ -1,1 +1,14 @@
-<!-- $database_name = "A_Agency_Client_Info_Sys"; -->
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database_name = "A_Agency_Client_Info_Sys";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database_name", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo"successfull connected";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
