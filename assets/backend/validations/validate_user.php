62bb8577950a2c8a6c6047803ac20049fb96a04a
Abdulaziz <abdulazizayalewmussa@gmail.com>
2025-05-04 11:35:17 +0300

    feat: add backend validation to ensure fullname is not empty

--- assets/backend/validations/validate_user.php
+++ assets/backend/validations/validate_user.php
@@ -2,6 +2,7 @@
 function validateUser($data) {
     $errors = [];
 
+    if (empty($data['fullname'])) $errors[] = "Full Name Can not be empty";
     if (empty($data['username'])) $errors[] = "Username is required.";
     if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) $errors[] = "Invalid email.";
     if (strlen($data['password']) < 6) $errors[] = "Password must be at least 6 characters.";
