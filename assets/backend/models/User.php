750f82d67888d726e016cdb19c8f3ed34b7ef7b5
Abdulaziz <abdulazizayalewmussa@gmail.com>
2025-05-04 19:39:45 +0300

    feat: Support storing passport no in User::Create() method

--- assets/backend/models/User.php
+++ assets/backend/models/User.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../config/database.php';
 
 class User {
-    public static function create($FullName,$username, $email, $password) {
+    public static function create($FullName,$Passport_no,$username, $email, $password) {
         $pdo = getPDO(); // Get the PDO instance
         // Check if email already exists
         $checkStmt = $pdo->prepare("SELECT id FROM users WHERE email = ?");
@@ -12,9 +12,10 @@ class User {
             // Email exists
             return false;
         }
-        $stmt = $pdo->prepare("INSERT INTO users (fullname,username, email, password) VALUES (?,?, ?, ?)");
+        $stmt = $pdo->prepare("INSERT INTO users (fullname,passport_no,username, email, password) VALUES (?, ?, ?, ?, ?)");
         return $stmt->execute([
             $FullName,
+            $Passport_no,
             $username,
             $email,
             password_hash($password, PASSWORD_DEFAULT)
